7c2a4d19a0ecf84e3a6e4db6f4301d02240ead00
lukashumer2002 <lukashumer2002@gmail.com>
2020-04-19 14:40:28 +0200

    BugFix

--- serviceApplication/src/app/app.module.ts
+++ serviceApplication/src/app/app.module.ts
@@ -16,7 +16,8 @@ import { TableComponent } from './table/table.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
-
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,7 @@ import { MatSortModule } from '@angular/material/sort';
     ServiceDetailComponent,
     PageNotFoundComponent,
     EmployeeDetailComponent,
-    TableComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
@@ -38,7 +39,9 @@ import { MatSortModule } from '@angular/material/sort';
     BrowserAnimationsModule,
     MatTableModule,
     MatPaginatorModule,
-    MatSortModule
+    MatSortModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [ServiceService],
   bootstrap: [AppComponent]
